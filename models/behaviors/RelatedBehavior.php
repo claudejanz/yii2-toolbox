0920b7c5ac914b3ee2ae148e36db5ddef1fe5001
Claude Janz <claude.janz@gmail.com>
2016-10-11 22:54:54 +0200

    Update to RelatedBehavior to delete existing and rebuild relations

--- models/behaviors/RelatedBehavior.php
+++ models/behaviors/RelatedBehavior.php
@@ -9,7 +9,8 @@ use yii\db\ActiveRecord;
  * @author Janz
  *
  */
-class RelatedBehavior extends Behavior {
+class RelatedBehavior extends Behavior
+{
     /**
      * @var string to recive data from form. in format "Arzier, Genève"
      * If you change the variable name please also change the $textFieldName value to match this public var
@@ -31,29 +32,32 @@ class RelatedBehavior extends Behavior {
     }
 
     public function afterSave($event) {
-        
+
         $model = $this->owner;
         foreach ($this->relations as $relationName => $relationValues) {
-            $relation = $model->getRelation($relationName);
-            $vias = $model->getRelation($relationName)->via;
+            $model->unlinkAll($relationName, true);
+            if (!empty($relationValues)) {
 
-            $modelKey = key(end($vias)->link);
-            $relationClassName = end($vias)->modelClass;
-            $relationKey=current ($relation->link);
-            foreach($relationValues as $value){
-                if($value instanceof ActiveRecord){
-                    $value = $value->primaryKey;
-                }
-                $attributes = [$modelKey=>$model->primaryKey,$relationKey=>$value];
-            
-                $pivot = $relationClassName::find()->where($attributes)->one();
-                if(!$pivot){
-                    $pivot = new $relationClassName;
-                    $pivot->attributes=$attributes;
-                    $pivot->save();
+                $relation = $model->getRelation($relationName);
+                $vias = $relation->via;
+
+                $modelKey = key(end($vias)->link);
+                $relationClassName = end($vias)->modelClass;
+                $relationKey = current($relation->link);
+                foreach ($relationValues as $value) {
+                    if ($value instanceof ActiveRecord) {
+                        $value = $value->primaryKey;
+                    }
+                    $attributes = [$modelKey => $model->primaryKey, $relationKey => $value];
+
+                    $pivot = $relationClassName::find()->where($attributes)->one();
+                    if (!$pivot) {
+                        $pivot = new $relationClassName;
+                        $pivot->attributes = $attributes;
+                        $pivot->save();
+                    }
                 }
             }
-            
         }
     }
 
