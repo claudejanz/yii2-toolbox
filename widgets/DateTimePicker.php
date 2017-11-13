95e96c0c9bce5920dc2ce6aa8219f0541c4af1af
Claude Janz <claude.janz@gmail.com>
2017-11-13 13:42:29 +0100

    13.11.2017

---
+++ widgets/DateTimePicker.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * @link https://www.klod.ch/ 
+ * @copyright Copyright (c) 2017 Klod SA
+ * @author Claude Janz <claude.janz@klod.ch>
+ */
+
+namespace claudejanz\toolbox\widgets;
+
+use claudejanz\toolbox\widgets\base\YiiInputWidget;
+
+/**
+ * Description of DateTimePicker
+ *
+ * @author Claude
+ */
+class DateTimePicker extends YiiInputWidget
+{
+    //put your code here
+}
---
+++ widgets/DateTimePickerAsset.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @copyright Copyright &copy; Kartik Visweswaran, Krajee.com, 2014 - 2017
+ * @package yii2-widgets
+ * @subpackage yii2-widget-datetimepicker
+ * @version 1.4.4
+ */
+
+namespace kartik\datetime;
+
+use kartik\base\AssetBundle;
+
+/**
+ * Asset bundle for [[DateTimePicker]] widget
+ *
+ * @author Kartik Visweswaran <kartikv2@gmail.com>
+ * @since 1.0
+ */
+class DateTimePickerAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/eternicode/bootstrap-datepicker';
+}
